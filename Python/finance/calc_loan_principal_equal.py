16554da9faa431c6b69b9fd26a61f95a95f49a4a
Keita Yamaguchi <nekton@fds-res.net>
2012-12-27 16:59:15 +0900

    add 元金均等

---
+++ Python/finance/calc_loan_principal_equal.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 元金均等
+
+# 元金均等返済方式(毎月返済, 端数切捨て)
+# @param debt           借入金
+# @param bonus          借入金の内、ボーナス返済の割合(未実装)
+# @param period_y       返済期間年
+# @param period_m       返済期間月
+# @param intrst         金利(当初金利)
+# @return [[支払い額, 元金充当,利息], ・・・]
+def calc_loan_principal_equal(debt, bonus, period_y, period_m, intrst):
+    result = []
+    pay_times = period_y * 12 + period_m
+    # 月利にする
+    calc_intrst = intrst / 100.0 / 12.0
+    a_appropriation = int(debt / pay_times)
+    print a_appropriation
+    principal = debt
+    for i in range(0, pay_times):
+        a_intrst = int(principal * calc_intrst)
+        principal = principal - a_appropriation
+        if i == pay_times-1:
+            a_appropriation = a_appropriation + principal
+            principal = 0
+        a_pay = a_appropriation + a_intrst
+        #print "%04d:%d=%d+%d %d" % (i, a_pay, a_appropriation, a_intrst, principal)
+        result.append([a_pay, a_appropriation, a_intrst])
+    return result
+
+if __name__ == "__main__":
+    result = calc_loan_principal_equal(10000000, 0, 10, 0, 2)
+    print result
+
+
